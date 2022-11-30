f0ded7ba65274d969bf7178b4ced5a5be6fa162b
Ayal <ayal.ciobotaru@caci.com>
2022-11-30 07:44:07 -0500

    removed unused code

--- Server/src/main/java/ac/myfinances/repo/TransactionRepository.java
+++ Server/src/main/java/ac/myfinances/repo/TransactionRepository.java
@@ -2,11 +2,7 @@ package ac.myfinances.repo;
 
 import ac.myfinances.rest.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import javax.transaction.Transactional;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
